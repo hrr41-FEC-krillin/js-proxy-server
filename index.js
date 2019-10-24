2af8e1909b545ef9f265c6867ccb27d83b12a78f
Jenny Shamoo <jenny.shamoo@gmail.com>
2019-10-23 22:07:49 -0700

    can render all components except critic reviews but not in nice format

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const Console = console;
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('tiny'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
-
-
 
 const port = 7000;
 app.listen(port, () => {
-  console.log(Date());
-  console.log(`Proxy server is listening on port ${port}`);
+  Console.log(Date());
+  Console.log(`Proxy server is listening on port ${port}`);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,32 @@
-module.exports = {
-  entry: {
+const path = require('path');
 
-  }
-}
+module.exports = {
+  mode: 'development',
+  entry: {
+    vendor: ['styled-components'],
+    photoCarousel: '../photo-carousel/client/components/stylesheet.jsx',
+    castPhotos: '../castphotos/client/styled.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  optimization: {
+    runtimeChunk: {
+      name: 'vendor',
+    },
+    splitChunks: {
+      cacheGroups: {
+        test: /[\\/]node_modules[\\/]/,
+        name: 'vendor',
+        chunks: 'all',
+        minSize: 0,
+      },
+    },
+    resolve: {
+      alias: {
+        'styled-components': path.resolve(__dirname, 'node_modules', 'styled-components'),
+      },
+    },
+  },
+};
