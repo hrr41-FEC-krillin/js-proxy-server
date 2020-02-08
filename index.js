55bca3a458add1365b5a3cceb7525150ffafd320
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-02-08 12:03:47 -0800

    added proxy server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
+const proxy = require('express-http-proxy');
 
 const Console = console;
 
@@ -12,6 +13,10 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('dist'));
+
+app.use('/api/cr_reviews', proxy('http://3.134.106.102:4540'));
+app.use('/api/movie', proxy('http:3.134.106.102:5050'));
+
 
 const port = 7000;
 app.listen(port, () => {
